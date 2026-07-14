bb90e3be3f0604df7fafcf4548bc34b1f51ca1b0
odkryjkobiecosc <krajewskaphoto@gmail.com>
2026-07-14 14:19:08 +0200

    Create sitemap.ts

---
+++ app/sitemap.ts
@@ -0,0 +1,29 @@
+// app/sitemap.ts
+
+import type { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const siteUrl = "https://www.odkryjkobiecosc.pl";
+  const currentDate = new Date();
+
+  return [
+    {
+      url: siteUrl,
+      lastModified: currentDate,
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: `${siteUrl}/polityka-prywatnosci`,
+      lastModified: currentDate,
+      changeFrequency: "yearly",
+      priority: 0.3,
+    },
+    {
+      url: `${siteUrl}/polityka-cookies`,
+      lastModified: currentDate,
+      changeFrequency: "yearly",
+      priority: 0.3,
+    },
+  ];
+}
