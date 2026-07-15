66336e1f117b01653e1d59a64afb6f173a2363ab
Marta Krajewska Photo <krajewskaphoto@gmail.com>
2026-07-15 13:57:42 +0200

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -20,6 +20,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.9,
     },
     {
+      url: `${siteUrl}/sesja-biznesowa-gdansk`,
+      lastModified: currentDate,
+      changeFrequency: "weekly",
+      priority: 0.85,
+    },
+    {
       url: `${siteUrl}/polityka-prywatnosci`,
       lastModified: currentDate,
       changeFrequency: "yearly",
